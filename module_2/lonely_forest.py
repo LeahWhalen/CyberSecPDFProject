f5a098e409abe1412f2a9a1d75fd8931bb9952a6
Leah Whalen <leah@launchbadge.com>
2022-10-30 16:36:11 -0700

    feat: add .gitignore

--- module_2/lonely_forest.py
+++ module_2/lonely_forest.py
@@ -10,12 +10,18 @@ class pdf_information:
     def __init__(self, pdf_text, pdf_strings):
         self.pdf = pdf.convert()
         self.pdf_strings = []
+        self.pdf_objects = []
 
+    # converts pdf to plain text?
     def convert(self, pdf):
         f = open(self.pdf, "r")
         pdf_as_str = f.read()
         self.pdf_strings = pdf_as_str.split(" ")
         print(pdf_strings)
 
+    # sort by objects
+    def convert_to_objects(self):
+
     def generate_lonely_forest(self):
-        
+        print(os.listdir("../input"))df=pd.read_csv("../input/metric_data.csv")
+        df.head()
